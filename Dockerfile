191dc074a907638f169c0f63dba0c262df1644a4
Vladimir Kruglov <vladimir.kruglov@gmail.com>
2019-11-26 13:43:38 +0300

    REFACTOR: Docker migrate django DB, view - remove temp file, template form action URL

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,12 @@ WORKDIR /app
 
 RUN conda install django
 RUN conda install gunicorn
-
 RUN conda install -c conda-forge djangorestframework
 
 COPY . /app
 
+RUN python /app/manage.py migrate
+
 EXPOSE 8000
+
+CMD ["gunicorn", "--chdir", "/app", "--bind", ":8000", "tubcheck.wsgi:application"]
--- oneimage/views.py
+++ oneimage/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from PIL import Image
 from tubcheck import ml_model
+import os
 
 
 def home_page(request):
@@ -16,5 +17,6 @@ def check_results(request):
     with open('tmp/image.jpg', 'rb') as fp:
         image = Image.open(fp)
         output = ml_model(image)
+    os.remove('tmp/image.jpg')
     return JsonResponse({'results': output['results']}, status=201)
 
